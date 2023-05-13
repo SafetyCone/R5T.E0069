67c6dbbfd32055053b9ae127ff220d9953f83f25
davidcoats <david@getsafetycone.com>
2023-05-13 14:26:24 -0700

    Initial commit.

---
+++ source/R5T.E0069.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.E0069.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.E0069.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.E0069.Construction
+Construction console project for the R5T.E0069 library.
